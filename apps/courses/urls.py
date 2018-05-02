455d58b74ce26a1275eba1c01427c2af94501faf
Chengtianlun <xyzlun@163.com>
2018-05-03 00:01:24 +0800

    添加课程详情页

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -3,10 +3,13 @@ __author__ = 'chengtianlun'
 __date__ = '2018/5/2 11:17'
 
 from django.conf.urls import url, include
-from views import CourseListView
+from views import CourseListView,CourseDetailView
 
 urlpatterns =  [
     # 课程机构首页
     url(r'^list/$', CourseListView.as_view(), name='course_list'),
+    # 课程详情页
+    url(r'^detail/(?P<course_id>\d+)/$', CourseDetailView.as_view(), name='course_detail'),
+
 
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -10,7 +10,16 @@ from models import Course
 class CourseListView(View):
     def get(self,request):
         current_page = 'course_list'
-        all_courses = Course.objects.all()
+        all_courses = Course.objects.all().order_by('-add_time')  # 减号表示降序排列
+
+        hot_courses =  Course.objects.all().order_by('-click_num')[:3]
+        #课程排序
+        sort = request.GET.get('sort', '')
+        if sort == 'students':
+            all_courses = all_courses.order_by('-students')
+        elif sort == 'hot':
+            all_courses = all_courses.order_by('-click_num')
+        course_nums = all_courses.count()
         # 对课程进行分页
         try:
             page = request.GET.get('page', 1)
@@ -22,4 +31,23 @@ class CourseListView(View):
         return render(request, 'course-list.html',{
             'all_courses' : courses,
             'current_page' : current_page,
+            'sort' : sort,
+            'hot_courses' : hot_courses
+        })
+
+
+class CourseDetailView(View):
+    '''
+    课程详情页
+    '''
+    def get(self,request,course_id):
+        course = Course.objects.get(id=int(course_id))
+        course.click_num += 1  # 增加课程点击数
+        course.save()
+        all_lesson = course.lesson_set.all()
+        lesson_num = all_lesson.count()
+        return render(request, 'course-detail.html',{
+            'course' : course,
+            'all_lesson' : all_lesson,
+            'lesson_num' : lesson_num,
         })
