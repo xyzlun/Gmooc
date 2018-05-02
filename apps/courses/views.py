4e1f7365a6176026f79017735679a0ab37e44ac5
xyzlun <xyzlun@163.com>
2018-05-02 17:29:15 +0800

    增加课程列表分页

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -2,6 +2,8 @@
 
 from django.shortcuts import render
 from django.views.generic.base import View
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
+
 from models import Course
 
 # Create your views here.
@@ -9,7 +11,15 @@ class CourseListView(View):
     def get(self,request):
         current_page = 'course_list'
         all_courses = Course.objects.all()
+        # 对课程进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_courses, 6, request=request)
+        courses = p.page(page)
         return render(request, 'course-list.html',{
-            'all_courses' : all_courses,
+            'all_courses' : courses,
             'current_page' : current_page,
         })
