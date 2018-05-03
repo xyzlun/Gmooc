bdaec918d4559afb1bfc1efb809d4bb95d33f5de
xyzlun <xyzlun@163.com>
2018-05-03 17:18:50 +0800

    数据库备份

--- mxonline.sql
+++ mxonline.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50721
 File Encoding         : 65001
 
-Date: 2018-05-02 17:30:25
+Date: 2018-05-03 17:17:15
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -264,21 +264,28 @@ CREATE TABLE `courses_course` (
   `click_num` int(11) NOT NULL,
   `add_time` datetime(6) NOT NULL,
   `course_org_id` int(11),
+  `category` varchar(20) NOT NULL,
+  `tag` varchar(10) NOT NULL,
+  `teacher_id` int(11),
+  `need_know` varchar(300) NOT NULL,
+  `teacher_tell` varchar(300) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `courses_course_11456c5a` (`course_org_id`),
-  CONSTRAINT `courses_cour_course_org_id_4d2c4aab_fk_organization_courseorg_id` FOREIGN KEY (`course_org_id`) REFERENCES `organization_courseorg` (`id`)
+  KEY `courses_course_d9614d40` (`teacher_id`),
+  CONSTRAINT `courses_cour_course_org_id_4d2c4aab_fk_organization_courseorg_id` FOREIGN KEY (`course_org_id`) REFERENCES `organization_courseorg` (`id`),
+  CONSTRAINT `courses_course_teacher_id_846fa526_fk_organization_teacher_id` FOREIGN KEY (`teacher_id`) REFERENCES `organization_teacher` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of courses_course
 -- ----------------------------
-INSERT INTO `courses_course` VALUES ('1', 'django入门', 'django入门', 'django入门', 'cj', '0', '0', '0', 'courses/2018/05/6aa513bae5e1ca06a6c43d942ffbfe47dd33eb0bc94a-Zvna7r_fw658.jpg', '0', '2018-04-08 15:02:00.000000', '1');
-INSERT INTO `courses_course` VALUES ('2', 'python入门', 'python入门', 'python入门', 'cj', '0', '0', '0', 'courses/2018/05/20180409111540_7vSlS4v.jpg', '0', '2018-05-02 16:16:00.000000', '2');
-INSERT INTO `courses_course` VALUES ('3', 'python入门2', 'python入门2', 'python入门2', 'zj', '0', '0', '0', 'courses/2018/05/b72954ee520a9a065bca726b57597fdad36beefe3e2bd-rFVGaH_fw658.jpg', '0', '2018-05-02 16:18:00.000000', '3');
-INSERT INTO `courses_course` VALUES ('4', 'python入门3', 'python入门3', 'python入门3', 'gj', '0', '0', '0', 'courses/2018/05/timg_4OSfqh2.jpg', '0', '2018-05-02 16:18:00.000000', '3');
-INSERT INTO `courses_course` VALUES ('5', 'python入门4', 'python入门4', 'python入门4', 'cj', '0', '0', '0', 'courses/2018/05/a6c40a978dfb0bbeae54d5c4bd8c732272e930d048893-brax02_fw658.jpg', '0', '2018-05-02 16:18:00.000000', '5');
-INSERT INTO `courses_course` VALUES ('6', 'python入门5', 'python入门5', 'python入门5', 'cj', '0', '0', '0', 'courses/2018/05/timg.jpg', '0', '2018-05-02 16:19:00.000000', '6');
-INSERT INTO `courses_course` VALUES ('7', 'python入门6', 'python入门6', 'python入门6', 'cj', '0', '0', '0', 'courses/2018/05/20180409111540.jpg', '0', '2018-05-02 16:19:00.000000', '1');
+INSERT INTO `courses_course` VALUES ('1', 'django入门', 'django入门', 'django入门', 'cj', '0', '0', '0', 'courses/2018/05/6aa513bae5e1ca06a6c43d942ffbfe47dd33eb0bc94a-Zvna7r_fw658.jpg', '40', '2018-04-08 15:02:00.000000', '1', '后端开发', 'python', '1', 'python基础', 'django进阶开发');
+INSERT INTO `courses_course` VALUES ('2', 'python入门', 'python入门', 'python入门', 'cj', '0', '0', '0', 'courses/2018/05/20180409111540_7vSlS4v.jpg', '2', '2018-05-02 16:16:00.000000', '2', '后端开发', 'python', '2', '', '');
+INSERT INTO `courses_course` VALUES ('3', 'python入门2', 'python入门2', 'python入门2', 'zj', '0', '0', '0', 'courses/2018/05/b72954ee520a9a065bca726b57597fdad36beefe3e2bd-rFVGaH_fw658.jpg', '0', '2018-05-02 16:18:00.000000', '3', '后端开发', '', null, '', '');
+INSERT INTO `courses_course` VALUES ('4', 'python入门3', 'python入门3', 'python入门3', 'gj', '0', '0', '0', 'courses/2018/05/timg_4OSfqh2.jpg', '0', '2018-05-02 16:18:00.000000', '3', '后端开发', '', null, '', '');
+INSERT INTO `courses_course` VALUES ('5', 'python入门4', 'python入门4', 'python入门4', 'cj', '0', '0', '0', 'courses/2018/05/a6c40a978dfb0bbeae54d5c4bd8c732272e930d048893-brax02_fw658.jpg', '0', '2018-05-02 16:18:00.000000', '5', '后端开发', '', null, '', '');
+INSERT INTO `courses_course` VALUES ('6', 'python入门5', 'python入门5', 'python入门5', 'cj', '0', '0', '0', 'courses/2018/05/timg.jpg', '14', '2018-05-02 16:19:00.000000', '6', '后端开发', '', null, '', '');
+INSERT INTO `courses_course` VALUES ('7', 'python入门6', 'python入门6', 'python入门6', 'cj', '0', '0', '0', 'courses/2018/05/20180409111540.jpg', '0', '2018-05-02 16:19:00.000000', '1', '后端开发', '', null, '', '');
 
 -- ----------------------------
 -- Table structure for courses_courseresource
@@ -293,11 +300,12 @@ CREATE TABLE `courses_courseresource` (
   PRIMARY KEY (`id`),
   KEY `courses_courseresource_course_id_5eba1332_fk_courses_course_id` (`course_id`),
   CONSTRAINT `courses_courseresource_course_id_5eba1332_fk_courses_course_id` FOREIGN KEY (`course_id`) REFERENCES `courses_course` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of courses_courseresource
 -- ----------------------------
+INSERT INTO `courses_courseresource` VALUES ('2', '前端页面', 'courses/resource/2018/05/mysql.jpg', '2018-05-03 16:39:00.000000', '1');
 
 -- ----------------------------
 -- Table structure for courses_lesson
@@ -311,12 +319,14 @@ CREATE TABLE `courses_lesson` (
   PRIMARY KEY (`id`),
   KEY `courses_lesson_course_id_16bc4882_fk_courses_course_id` (`course_id`),
   CONSTRAINT `courses_lesson_course_id_16bc4882_fk_courses_course_id` FOREIGN KEY (`course_id`) REFERENCES `courses_course` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of courses_lesson
 -- ----------------------------
 INSERT INTO `courses_lesson` VALUES ('1', '第一章', '2018-04-08 15:05:00.000000', '1');
+INSERT INTO `courses_lesson` VALUES ('2', '第二章', '2018-05-03 15:35:00.000000', '1');
+INSERT INTO `courses_lesson` VALUES ('3', '第三章', '2018-05-03 15:35:00.000000', '1');
 
 -- ----------------------------
 -- Table structure for courses_video
@@ -327,14 +337,21 @@ CREATE TABLE `courses_video` (
   `name` varchar(100) NOT NULL,
   `add_time` datetime(6) NOT NULL,
   `lesson_id` int(11) NOT NULL,
+  `url` varchar(200) NOT NULL,
+  `learn_times` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `courses_video_lesson_id_59f2396e_fk_courses_lesson_id` (`lesson_id`),
   CONSTRAINT `courses_video_lesson_id_59f2396e_fk_courses_lesson_id` FOREIGN KEY (`lesson_id`) REFERENCES `courses_lesson` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of courses_video
 -- ----------------------------
+INSERT INTO `courses_video` VALUES ('1', '第1.1节', '2018-05-03 15:37:00.000000', '1', 'http://www.xyzlun.me', '0');
+INSERT INTO `courses_video` VALUES ('2', '第2.1节', '2018-05-03 15:37:00.000000', '2', 'http://www.xyzlun.me', '0');
+INSERT INTO `courses_video` VALUES ('3', '第3.1节', '2018-05-03 15:37:00.000000', '3', 'http://www.xyzlun.me', '0');
+INSERT INTO `courses_video` VALUES ('4', '第1.2节', '2018-05-03 15:59:00.000000', '1', 'http://www.xyzlun.me', '0');
+INSERT INTO `courses_video` VALUES ('5', '第2.2节', '2018-05-03 15:59:00.000000', '2', 'http://www.xyzlun.me', '0');
 
 -- ----------------------------
 -- Table structure for django_admin_log
@@ -412,7 +429,7 @@ CREATE TABLE `django_migrations` (
   `name` varchar(255) NOT NULL,
   `applied` datetime(6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of django_migrations
@@ -450,6 +467,12 @@ INSERT INTO `django_migrations` VALUES ('30', 'organization', '0004_auto_2018042
 INSERT INTO `django_migrations` VALUES ('31', 'courses', '0003_course_course_org', '2018-04-27 09:32:31.781000');
 INSERT INTO `django_migrations` VALUES ('32', 'organization', '0005_teacher_image', '2018-04-27 11:31:11.016000');
 INSERT INTO `django_migrations` VALUES ('33', 'organization', '0006_teacher_course_num', '2018-04-28 15:09:54.431000');
+INSERT INTO `django_migrations` VALUES ('34', 'courses', '0004_course_category', '2018-05-03 09:14:42.689000');
+INSERT INTO `django_migrations` VALUES ('35', 'courses', '0005_course_tag', '2018-05-03 09:53:00.764000');
+INSERT INTO `django_migrations` VALUES ('36', 'courses', '0006_video_url', '2018-05-03 15:56:56.757000');
+INSERT INTO `django_migrations` VALUES ('37', 'courses', '0007_video_learn_times', '2018-05-03 16:28:49.993000');
+INSERT INTO `django_migrations` VALUES ('38', 'courses', '0008_auto_20180503_1652', '2018-05-03 16:53:03.828000');
+INSERT INTO `django_migrations` VALUES ('39', 'courses', '0009_auto_20180503_1713', '2018-05-03 17:13:33.973000');
 
 -- ----------------------------
 -- Table structure for django_session
@@ -466,7 +489,7 @@ CREATE TABLE `django_session` (
 -- ----------------------------
 -- Records of django_session
 -- ----------------------------
-INSERT INTO `django_session` VALUES ('5cnui5jcilg37av3xbipk32dki12ru37', 'ZThmY2VjNmE3NTNiYjVlZDVkZjc5MTUyNDNlMjBhZmE5NWNlN2Y3MDp7Il9hdXRoX3VzZXJfaWQiOiI0IiwiTElTVF9RVUVSWSI6W1siY291cnNlcyIsImNvdXJzZSJdLCIiXSwiX2F1dGhfdXNlcl9oYXNoIjoiYjliZTg3ZTdmZmVjY2RjMjkxODk4NWNjODdjZDcwNzQxMTdmNTY2MiIsIl9hdXRoX3VzZXJfYmFja2VuZCI6InVzZXJzLnZpZXdzLkN1c3RvbUJhY2tlbmQifQ==', '2018-05-16 16:21:29.309000');
+INSERT INTO `django_session` VALUES ('5cnui5jcilg37av3xbipk32dki12ru37', 'ZThmY2VjNmE3NTNiYjVlZDVkZjc5MTUyNDNlMjBhZmE5NWNlN2Y3MDp7Il9hdXRoX3VzZXJfaWQiOiI0IiwiTElTVF9RVUVSWSI6W1siY291cnNlcyIsImNvdXJzZSJdLCIiXSwiX2F1dGhfdXNlcl9oYXNoIjoiYjliZTg3ZTdmZmVjY2RjMjkxODk4NWNjODdjZDcwNzQxMTdmNTY2MiIsIl9hdXRoX3VzZXJfYmFja2VuZCI6InVzZXJzLnZpZXdzLkN1c3RvbUJhY2tlbmQifQ==', '2018-05-17 17:15:09.735000');
 INSERT INTO `django_session` VALUES ('u6qb1oe4uza2qqseblmv1ebsad4mdytf', 'MTM4YTU2NzA3ODE5YTY1NTgxZDMxZDMwMzNhNDZlZjE1NzgzNjMyMjp7Il9hdXRoX3VzZXJfaGFzaCI6IjMxY2ZjZTFhOWM1ZDIwOGQ2YWQwYzFiNDg4ZTk0NWY3ZGQyYjliYTMiLCJfYXV0aF91c2VyX2JhY2tlbmQiOiJ1c2Vycy52aWV3cy5DdXN0b21CYWNrZW5kIiwiX2F1dGhfdXNlcl9pZCI6IjExIn0=', '2018-04-26 17:31:43.084000');
 
 -- ----------------------------
@@ -521,11 +544,14 @@ CREATE TABLE `operation_usercourse` (
   KEY `operation_usercourse_user_id_835fe81a_fk_users_userprofile_id` (`user_id`),
   CONSTRAINT `operation_usercourse_course_id_9f1eab2e_fk_courses_course_id` FOREIGN KEY (`course_id`) REFERENCES `courses_course` (`id`),
   CONSTRAINT `operation_usercourse_user_id_835fe81a_fk_users_userprofile_id` FOREIGN KEY (`user_id`) REFERENCES `users_userprofile` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of operation_usercourse
 -- ----------------------------
+INSERT INTO `operation_usercourse` VALUES ('1', '2018-05-03 09:16:00.000000', '1', '4');
+INSERT INTO `operation_usercourse` VALUES ('2', '2018-05-03 09:16:00.000000', '1', '11');
+INSERT INTO `operation_usercourse` VALUES ('3', '2018-05-03 09:16:00.000000', '2', '11');
 
 -- ----------------------------
 -- Table structure for operation_userfavorite
@@ -540,12 +566,12 @@ CREATE TABLE `operation_userfavorite` (
   PRIMARY KEY (`id`),
   KEY `operation_userfavorite_user_id_ad46a6af_fk_users_userprofile_id` (`user_id`),
   CONSTRAINT `operation_userfavorite_user_id_ad46a6af_fk_users_userprofile_id` FOREIGN KEY (`user_id`) REFERENCES `users_userprofile` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of operation_userfavorite
 -- ----------------------------
-INSERT INTO `operation_userfavorite` VALUES ('2', '1', '2', '2018-05-02 09:21:19.299000', '4');
+INSERT INTO `operation_userfavorite` VALUES ('9', '1', '2', '2018-05-03 11:22:08.645000', '4');
 
 -- ----------------------------
 -- Table structure for operation_usermessage
@@ -723,10 +749,10 @@ CREATE TABLE `users_userprofile` (
 -- ----------------------------
 -- Records of users_userprofile
 -- ----------------------------
-INSERT INTO `users_userprofile` VALUES ('1', 'pbkdf2_sha256$24000$CNfQRdrGtbvN$xY4wN+62y2JHT/LOHgsH2wq0gnEtC04D4VsewnIssns=', '2018-04-20 09:12:24.316000', '1', 'admin', '', '', '1@1.com', '1', '1', '2018-04-04 07:23:42.050000', '', null, 'female', '', null, 'image/default.png');
-INSERT INTO `users_userprofile` VALUES ('4', 'pbkdf2_sha256$24000$b6ISDzYGJ0zn$FVl22g4OWlSvk6Hmz8apfiuvlZZ9wcJdbzHbSCib8u0=', '2018-05-02 09:29:49.238000', '1', 'chengtianlun', '', '', '2@2.com', '1', '1', '2018-04-04 16:24:23.856000', '', null, 'female', '', null, 'image/default.png');
-INSERT INTO `users_userprofile` VALUES ('7', 'admin123', null, '0', 'yyx', '', '', '', '0', '1', '2018-04-04 16:37:00.000000', 'yyx', null, 'female', 'chengdu', '', 'image/default.png');
-INSERT INTO `users_userprofile` VALUES ('11', 'pbkdf2_sha256$24000$axXGqfAXpWJx$1iDMMOUJNAZ7T7gL3bDzPu/xQMJqHEdfPLAHOt/U6wE=', '2018-04-16 16:31:30.833000', '0', '450347106@qq.com', '', '', '450347106@qq.com', '0', '1', '2018-04-12 16:44:11.558000', '', null, 'female', '', null, 'image/default.png');
+INSERT INTO `users_userprofile` VALUES ('1', 'pbkdf2_sha256$24000$CNfQRdrGtbvN$xY4wN+62y2JHT/LOHgsH2wq0gnEtC04D4VsewnIssns=', '2018-04-20 09:12:00.000000', '1', 'admin', '', '', '1@1.com', '1', '1', '2018-04-04 07:23:00.000000', 'admin', null, 'female', '北京', '', 'image/2018/05/b72954ee520a9a065bca726b57597fdad36beefe3e2bd-rFVGaH_fw658.jpg');
+INSERT INTO `users_userprofile` VALUES ('4', 'pbkdf2_sha256$24000$b6ISDzYGJ0zn$FVl22g4OWlSvk6Hmz8apfiuvlZZ9wcJdbzHbSCib8u0=', '2018-05-02 09:29:00.000000', '1', 'chengtianlun', '', '', '2@2.com', '1', '1', '2018-04-04 16:24:00.000000', 'chengtianlun', null, 'male', '成都', '', 'image/2018/05/curry.jpg');
+INSERT INTO `users_userprofile` VALUES ('7', 'admin123', null, '0', 'yyx', '', '', '', '0', '1', '2018-04-04 16:37:00.000000', 'yyx', null, 'female', 'chengdu', '', 'image/2018/05/timg.jpg');
+INSERT INTO `users_userprofile` VALUES ('11', 'pbkdf2_sha256$24000$axXGqfAXpWJx$1iDMMOUJNAZ7T7gL3bDzPu/xQMJqHEdfPLAHOt/U6wE=', '2018-04-16 16:31:00.000000', '0', '450347106@qq.com', '', '', '450347106@qq.com', '0', '1', '2018-04-12 16:44:00.000000', '450347106', null, 'female', '杭州', '', 'image/2018/05/20180409111540.jpg');
 
 -- ----------------------------
 -- Table structure for users_userprofile_groups
@@ -808,7 +834,7 @@ CREATE TABLE `xadmin_log` (
   KEY `xadmin_log_user_id_bb16a176_fk_users_userprofile_id` (`user_id`),
   CONSTRAINT `xadmin_log_content_type_id_2a6cb852_fk_django_content_type_id` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
   CONSTRAINT `xadmin_log_user_id_bb16a176_fk_users_userprofile_id` FOREIGN KEY (`user_id`) REFERENCES `users_userprofile` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=44 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=73 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of xadmin_log
@@ -856,6 +882,35 @@ INSERT INTO `xadmin_log` VALUES ('40', '2018-05-02 16:21:05.432000', '127.0.0.1'
 INSERT INTO `xadmin_log` VALUES ('41', '2018-05-02 16:21:12.443000', '127.0.0.1', '3', 'python入门2', 'change', '已修改 image 。', '10', '4');
 INSERT INTO `xadmin_log` VALUES ('42', '2018-05-02 16:21:18.826000', '127.0.0.1', '2', 'python入门', 'change', '已修改 image 。', '10', '4');
 INSERT INTO `xadmin_log` VALUES ('43', '2018-05-02 16:21:28.967000', '127.0.0.1', '1', 'django入门', 'change', '已修改 image 。', '10', '4');
+INSERT INTO `xadmin_log` VALUES ('44', '2018-05-03 09:16:11.853000', '127.0.0.1', '1', 'UserCourse object', 'create', '已添加', '21', '4');
+INSERT INTO `xadmin_log` VALUES ('45', '2018-05-03 09:16:20.232000', '127.0.0.1', '2', 'UserCourse object', 'create', '已添加', '21', '4');
+INSERT INTO `xadmin_log` VALUES ('46', '2018-05-03 09:16:32.011000', '127.0.0.1', '3', 'UserCourse object', 'create', '已添加', '21', '4');
+INSERT INTO `xadmin_log` VALUES ('47', '2018-05-03 09:18:32.579000', '127.0.0.1', '11', '450347106@qq.com', 'change', '已修改 last_login，nick_name，address 和 image 。', '7', '4');
+INSERT INTO `xadmin_log` VALUES ('48', '2018-05-03 09:18:52.306000', '127.0.0.1', '4', 'chengtianlun', 'change', '已修改 last_login，nick_name，gender，address 和 image 。', '7', '4');
+INSERT INTO `xadmin_log` VALUES ('49', '2018-05-03 09:19:02.773000', '127.0.0.1', '7', 'yyx', 'change', '已修改 image 。', '7', '4');
+INSERT INTO `xadmin_log` VALUES ('50', '2018-05-03 09:19:29.126000', '127.0.0.1', '1', 'admin', 'change', '已修改 last_login，nick_name，address 和 image 。', '7', '4');
+INSERT INTO `xadmin_log` VALUES ('51', '2018-05-03 15:35:30.018000', '127.0.0.1', '2', '第一章', 'create', '已添加', '11', '4');
+INSERT INTO `xadmin_log` VALUES ('52', '2018-05-03 15:35:41.033000', '127.0.0.1', '3', '第二章', 'create', '已添加', '11', '4');
+INSERT INTO `xadmin_log` VALUES ('53', '2018-05-03 15:35:52.015000', '127.0.0.1', '2', '第二章', 'change', '已修改 name 。', '11', '4');
+INSERT INTO `xadmin_log` VALUES ('54', '2018-05-03 15:35:58.473000', '127.0.0.1', '3', '第三章', 'change', '已修改 name 。', '11', '4');
+INSERT INTO `xadmin_log` VALUES ('55', '2018-05-03 15:37:25.403000', '127.0.0.1', '1', '第一节', 'create', '已添加', '12', '4');
+INSERT INTO `xadmin_log` VALUES ('56', '2018-05-03 15:37:41.201000', '127.0.0.1', '2', '第一节', 'create', '已添加', '12', '4');
+INSERT INTO `xadmin_log` VALUES ('57', '2018-05-03 15:37:56.180000', '127.0.0.1', '3', '第三节', 'create', '已添加', '12', '4');
+INSERT INTO `xadmin_log` VALUES ('58', '2018-05-03 15:39:16.034000', '127.0.0.1', '3', '第3.1节', 'change', '已修改 name 。', '12', '4');
+INSERT INTO `xadmin_log` VALUES ('59', '2018-05-03 15:39:22.250000', '127.0.0.1', '2', '第2.1节', 'change', '已修改 name 。', '12', '4');
+INSERT INTO `xadmin_log` VALUES ('60', '2018-05-03 15:39:28.239000', '127.0.0.1', '1', '第1.1节', 'change', '已修改 name 。', '12', '4');
+INSERT INTO `xadmin_log` VALUES ('61', '2018-05-03 15:57:41.399000', '127.0.0.1', '3', '第3.1节', 'change', '已修改 url 。', '12', '4');
+INSERT INTO `xadmin_log` VALUES ('62', '2018-05-03 15:57:46.817000', '127.0.0.1', '2', '第2.1节', 'change', '已修改 url 。', '12', '4');
+INSERT INTO `xadmin_log` VALUES ('63', '2018-05-03 15:57:50.828000', '127.0.0.1', '1', '第1.1节', 'change', '已修改 url 。', '12', '4');
+INSERT INTO `xadmin_log` VALUES ('64', '2018-05-03 15:59:34.397000', '127.0.0.1', '4', '第1.2节', 'create', '已添加', '12', '4');
+INSERT INTO `xadmin_log` VALUES ('65', '2018-05-03 15:59:50.179000', '127.0.0.1', '5', '第2.2节', 'create', '已添加', '12', '4');
+INSERT INTO `xadmin_log` VALUES ('66', '2018-05-03 16:37:48.673000', '127.0.0.1', '1', '前端页面', 'create', '已添加', '13', '4');
+INSERT INTO `xadmin_log` VALUES ('67', '2018-05-03 16:38:58.401000', '127.0.0.1', '1', '前端页面', 'change', '没有字段被修改。', '13', '4');
+INSERT INTO `xadmin_log` VALUES ('68', '2018-05-03 16:39:29.750000', '127.0.0.1', '1', '前端页面', 'delete', '', '13', '4');
+INSERT INTO `xadmin_log` VALUES ('69', '2018-05-03 16:39:55.445000', '127.0.0.1', '2', '前端页面', 'create', '已添加', '13', '4');
+INSERT INTO `xadmin_log` VALUES ('70', '2018-05-03 16:53:48.032000', '127.0.0.1', '1', 'django入门', 'change', '已修改 teacher 。', '10', '4');
+INSERT INTO `xadmin_log` VALUES ('71', '2018-05-03 16:53:55.602000', '127.0.0.1', '2', 'python入门', 'change', '已修改 teacher 。', '10', '4');
+INSERT INTO `xadmin_log` VALUES ('72', '2018-05-03 17:15:09.403000', '127.0.0.1', '1', 'django入门', 'change', '已修改 need_know 和 teacher_tell 。', '10', '4');
 
 -- ----------------------------
 -- Table structure for xadmin_usersettings
