2fc6d336c73d9abf18f6baf026f0fdd77f245e2c
Chengtianlun <xyzlun@163.com>
2018-05-07 22:19:54 +0800

    权限验证

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -5,6 +5,7 @@ from django.views.generic.base import View
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from operation.models import UserFavorite,CourseComments,UserCourse
 from django.http import HttpResponse
+from utils.mixin_utils import LoginRequiredMixin
 import json
 
 
@@ -72,18 +73,24 @@ class CourseDetailView(View):
         })
 
 
-class CourseInfoView(View):
+class CourseInfoView(LoginRequiredMixin,View):
     '''
     课程章节信息
     '''
     def get(self, request, course_id):
         current_page = 'course_list'
         course = Course.objects.get(id=int(course_id))
+        #查询用户是否已经关联该课程
+        user_courses = UserCourse.objects.filter(user=request.user, course=course)
+        if not user_courses:
+            user_course = UserCourse(user=request.user, course=course)
+            user_course.save()
+
         user_courses = UserCourse.objects.filter(course=course)
         user_ids = [user_course.user.id for user_course in user_courses]
         all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
         #取出所有课程id
-        course_ids = [user_course.course.id for user_course in user_courses]
+        course_ids = [user_course.course.id for user_course in all_user_courses]
         #获取学过该课程的其他用户学习的课程（取点击量前五）
         relate_courses = Course.objects.filter(id__in=course_ids).order_by('-click_num')[:5]
         all_resources = CourseResource.objects.filter(course=course)
@@ -95,18 +102,23 @@ class CourseInfoView(View):
         })
 
 
-class CommentsView(View):
+class CommentsView(LoginRequiredMixin,View):
     '''
     课程评论
     '''
     def get(self, request, course_id):
         current_page = 'course_list'
         course = Course.objects.get(id=int(course_id))
+        #查询用户是否已经关联该课程
+        user_courses = UserCourse.objects.filter(user=request.user, course=course)
+        if not user_courses:
+            user_course = UserCourse(user=request.user, course=course)
+            user_course.save()
         user_courses = UserCourse.objects.filter(course=course)
         user_ids = [user_course.user.id for user_course in user_courses]
         all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
         #取出所有课程id
-        course_ids = [user_course.course.id for user_course in user_courses]
+        course_ids = [user_course.course.id for user_course in all_user_courses]
         #获取学过该课程的其他用户学习的课程（取点击量前五）
         relate_courses = Course.objects.filter(id__in=course_ids).order_by('-click_num')[:5]
         all_resources = CourseResource.objects.filter(course=course)
---
+++ apps/utils/mixin_utils.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'chengtianlun'
+__date__ = '2018/5/5 22:24'
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+
+class LoginRequiredMixin(object):
+
+    @method_decorator(login_required(login_url='/login/'))
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginRequiredMixin,self).dispatch(request, *args, **kwargs)
